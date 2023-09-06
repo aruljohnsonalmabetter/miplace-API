c0b28570b6aa42473013f60c5d752ad85f53fe04
[Arul Johnson] <arul.johnson@almabetter.com>
2023-09-07 01:15:58 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const PORT = process.env.PORT || 8000;
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express ();
 app.use(express.json());
+
+//My routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:",
