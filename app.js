185b9b8a51f0e6e63b52491adfedfb6777eda87a
[Arul Johnson] <arul.johnson@almabetter.com>
2023-09-06 12:33:04 +0530

    new

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+
+const app = express ();
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:",
+    {PORT} );
+  });
+
