68f6052037ed55429ae203178c2f9a351ceed7e4
Rudransh <rudrcodes@gmail.com>
2023-09-22 01:52:14 +0530

    Stripe Gateway Addwd - Working Correcty now

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-const app = express ();
-// app.use(express.json());
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 //My routes
 const authRoutes = require("./routes/auth");
@@ -15,13 +20,45 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //My Routes
+
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 
-
+//checkout api
+app.post("/api/create-checkout-session", async (req, res) => {
+  // console.log(req.body);
+  const { products } = req.body;
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "inr", // Change to your desired currency
+          product_data: {
+            name: products[0].hotel_name,
+            images: [products[0].main_photo_url],
+          },
+          // currencyPrice * (noOfDays + 1) * rooms
+          unit_amount: parseInt(
+            products[0].currencyPrice *
+              (products[0].noOfDays + 1) *
+              products[0].rooms *
+              100
+          ),
+          // unit_amount: products[0].currencyPrice * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    // success_url: "https://www.youtube.com/",
+    success_url: "http://localhost:3000/mybookings",
+    cancel_url: "http://localhost:3000/error",
+  });
+  res.json({ id: session.id });
+  console.log(products);
+});
 
 app.listen(PORT, () => {
-    console.log("Server Listening on PORT:",
-    {PORT} );
-  });
-
+  console.log("Server Listening on PORT:", { PORT });
+});
